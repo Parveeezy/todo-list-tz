269ca48d1070cb0a18b7010a6d0d4f08531a1bcb
Parviz Shirinov <parveeezy@gmail.com>
2022-10-17 16:10:59 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {v4} from "uuid";
+import Input from "./Input/Input";
+import TodoBody from "./TodoBody/TodoBody";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [todo, setTodo] = useState('')
+    const [todos, setTodos] = useState([])
+    const [todoEditing, setTodoEditing] = useState(null)
+    const [editingText, setEditingText] = useState('')
+
+    useEffect(() => {
+        const temp = window.localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+
+        if (loadedTodos) {
+            setTodos(loadedTodos)
+        }
+    }, [])
+
+    useEffect(() => {
+        const temp = JSON.stringify(todos)
+        window.localStorage.setItem('todos', temp)
+    }, [todos])
+
+    const newTodo = {
+        id: v4(),
+        text: todo,
+        isDone: false,
+        editing: false
+    }
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        if (todo !== '') {
+            setTodos([...todos, newTodo])
+            setTodo('')
+        }
+    }
+    const onChangeHandler = (e) => {
+        setTodo(e.currentTarget.value)
+    }
+    const removeTask = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+    const checkedHandler = (id) => {
+        setTodos(todos.map(todo => todo.id === id ?
+            {...todo, isDone: !todo.isDone} : todo))
+    }
+    const editTodo = (id) => {
+        const updatedTodos = [...todos].map(todo => {
+            if (todo.id === id) {
+                todo.text = editingText
+                todo.editing = !todo.editing
+            }
+            return todo
+        })
+        if (todo.text !== '') {
+            setTodos(updatedTodos)
+            setTodoEditing(null)
+            setEditingText('')
+        }
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <Input todo={todo}
+                       addTodo={addTodo}
+                       onChangeHandler={onChangeHandler}
+                />
+
+                <TodoBody todos={todos}
+                          removeTask={removeTask}
+                          checkedHandler={checkedHandler}
+                          todoEditing={todoEditing}
+                          setTodoEditing={setTodoEditing}
+                          editingText={editingText}
+                          setEditingText={setEditingText}
+                          editTodo={editTodo}
+                />
+            </div>
+        </div>
+    )
+
 }
 
 export default App;
